afac61c5e5426599d78b9ae3eff342d65db8e164
yberthier <yberthier@upmem.com>
2024-08-05 14:07:16 +0000

    update c app

--- c_program/dpu/dpu.c
+++ c_program/dpu/dpu.c
@@ -34,47 +34,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "common.h"
-
-/* Use blocks of 256 bytes */
-#define BLOCK_SIZE (256)
-
-__dma_aligned uint8_t DPU_CACHES[NR_TASKLETS][BLOCK_SIZE];
-__host dpu_results_t DPU_RESULTS;
-
-__mram_noinit uint8_t DPU_BUFFER[BUFFER_SIZE];
-
 /**
  * @fn main
  * @brief main function executed by each tasklet
- * @return the checksum result
  */
 int main()
 {
-    uint32_t tasklet_id = me();
-    uint8_t *cache = DPU_CACHES[tasklet_id];
-    dpu_result_t *result = &DPU_RESULTS.tasklet_result[tasklet_id];
-    uint32_t checksum = 0;
-
-    /* Initialize once the cycle counter */
-    if (tasklet_id == 0)
-        perfcounter_config(COUNT_CYCLES, true);
-
-    for (uint32_t buffer_idx = tasklet_id * BLOCK_SIZE; buffer_idx < BUFFER_SIZE; buffer_idx += (NR_TASKLETS * BLOCK_SIZE)) {
-
-        /* load cache with current mram block. */
-        mram_read(&DPU_BUFFER[buffer_idx], cache, BLOCK_SIZE);
-
-        /* computes the checksum of a cached block */
-        for (uint32_t cache_idx = 0; cache_idx < BLOCK_SIZE; cache_idx++) {
-            checksum += cache[cache_idx];
-        }
-    }
-
-    /* keep the 32-bit LSB on the 64-bit cycle counter */
-    result->cycles = (uint32_t)perfcounter_get();
-    result->checksum = checksum;
-
-    printf("[%02d] Checksum = 0x%08x\n", tasklet_id, result->checksum);
     return 0;
 }
